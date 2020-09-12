c81942e7f5a0de05c421b57d4a3a4227ca49634b
Chris Wu <cwu0569@gmail.com>
2020-09-12 04:24:07 -0700

    menu page in progress

--- src/index.js
+++ src/index.js
@@ -3,6 +3,19 @@ import {initialHomeLoad, createHeader} from './home-tab';
 // home page controller
 window.addEventListener('load', () => {
     createHeader();
-    initialHomeLoad();
+    // initialHomeLoad();
+    // setActiveTab('home');
 });
 
+// tabs controller
+const setActiveTab = tab => {
+    const tabsDOM = {
+        home: document.querySelector('#home-tab'),
+        menu: document.querySelector('#menu-tab'),
+        contact: document.querySelector('#contact-tab')
+    };
+
+    let currentTab;
+    tab === 'home' ? currentTab = tabsDOM.home : tab === 'menu' ? currentTab = tabsDOM.menu : currentTab = tabsDOM.contact;
+    currentTab.classList.add('tabs-active');
+};
